0b67d0f15b8e9bdd16b350a62351a650cc834c43
dwfig <daniel.w.fries@gmail.com>
2019-06-24 19:58:06 -0400

    added transitions

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
   console.log("loaded")
-  let carousel = document.querySelector(".image")
+  let carouselTop = document.querySelector("#first-image")
+  let carouselBot = document.querySelector("#second-image")
   let carouselIndex = 0
   setInterval(() =>{
     console.log("2s");
@@ -8,6 +9,19 @@ document.addEventListener("DOMContentLoaded", (e) =>{
     if (carouselIndex > 3){
       carouselIndex = 0
     };
-    carousel.style.backgroundImage = `url("./images/hero-image-${carouselIndex}.jpg")`
+    // carouselBot.style.backgroundImage = `url("./images/hero-image-${carouselIndex}.jpg")`
+    // carouselTop.style.opacity = "0"
+    carouselTop.style.backgroundImage = `url("./images/hero-image-${carouselIndex}.jpg")`
+    // carouselTop.style.opacity = "1"
   },2000)
+  // setInterval(()=>{
+  //
+  // },3000)
 })
+
+// two images :
+// fade out top image
+// change top image
+// fade in top image
+// change bottom image
+// repeat
