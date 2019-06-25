ed0df03d94fdb7008b1392ee89d7fefa58680e20
dwfig <daniel.w.fries@gmail.com>
2019-06-25 09:02:43 -0400

    removed second image, cleaned comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e) =>{
   console.log("loaded")
   let carouselTop = document.querySelector("#first-image")
-  let carouselBot = document.querySelector("#second-image")
   let carouselIndex = 0
   setInterval(() =>{
     console.log("2s");
@@ -9,19 +8,6 @@ document.addEventListener("DOMContentLoaded", (e) =>{
     if (carouselIndex > 3){
       carouselIndex = 0
     };
-    // carouselBot.style.backgroundImage = `url("./images/hero-image-${carouselIndex}.jpg")`
-    // carouselTop.style.opacity = "0"
     carouselTop.style.backgroundImage = `url("./images/hero-image-${carouselIndex}.jpg")`
-    // carouselTop.style.opacity = "1"
   },2000)
-  // setInterval(()=>{
-  //
-  // },3000)
 })
-
-// two images :
-// fade out top image
-// change top image
-// fade in top image
-// change bottom image
-// repeat
